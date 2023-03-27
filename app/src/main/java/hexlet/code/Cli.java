b4919686b595935d4769a366b5510673c038a87b
Grigorii Utiugov <grigorii.utiugov@nordclan.com>
2023-03-27 21:55:52 +0300

    add checkstyle and fix linter bugs

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,12 +4,12 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Cli {
-    
+
     public static void greetings() {
         System.out.println("Welcome to the Brain Games!\nMay I have your name?");
         Scanner scanner = new Scanner(new InputStreamReader(System.in));
         String name = scanner.nextLine();
         System.out.printf("Hello, %s!%n", name);
     }
-    
+
 }
