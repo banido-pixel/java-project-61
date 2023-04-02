1a96d58d19faa0ed8f587e77ba4ab356fd29b3fa
Grigorii Utiugov <grigorii.utiugov@nordclan.com>
2023-04-02 22:43:49 +0300

    refactor EvenGame

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,39 @@
 package hexlet.code;
 
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class App {
+
+    private static Scanner scanner;
+    private static final String GAMES_INFO = """
+            Please enter the game number and press Enter.
+            1 - Greet
+            2 - Even
+            0 - Exit""";
+
     public static void main(String[] args) {
-        Cli.start();
+        start();
+    }
+
+    public static void start() {
+        scanner = new Scanner(new InputStreamReader(System.in));
+        boolean toExit = false;
+        while (!toExit) {
+            System.out.println(GAMES_INFO);
+            String choice = scanner.nextLine();
+            switch (choice) {
+                case ("1") -> Cli.greetings();
+                case ("2") -> EvenGame.start();
+                case ("0") -> toExit = true;
+                default -> {
+                }
+            }
+        }
+        scanner.close();
+    }
+
+    public static Scanner getScanner() {
+        return scanner;
     }
 }
